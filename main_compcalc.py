c4de2ef01828562f0b6242d44761a3a8746f4230
Eike <eike2d85@gmail.com>
2022-09-20 17:39:44 -0300

    relampago

--- main_compcalc.py
+++ main_compcalc.py
@@ -1,15 +1,21 @@
 import numpy as np
 from regmist_input import regmist_input
-
-#---------------DADOS DE ENTRADA---------------
+'''
+OBJETIVO: Plotar a superfície de falha pelos 3 critérios e mostrar o ponto de cada lâmina. Printar se cada lâmina falhou ou não e o tipo da falha.
+    -> Entrada pelas propriedades diretas ou regra das misturas;
+    
+CONSIDERAÇÕES:
+    -> Todas as lâminas do mesmo material;
+'''
+#----------------DADOS DE ENTRADA----------------
 # Opções de entrada: 0-> Propriedades do material; 1->Regra das Misturas
 ent_opt = 0
 
 if ent_opt == 0:
-    E1 = 1
-    E2 = 1
+    E1 = 40000 # MPa
+    E2 = 3000 # MPa
     v12 = 1
-    G12 = 1
+    G12 = 10000 # MPa
     inputs = np.array([E1, E2, v12, G12])
 elif ent_opt == 1:
     Ef = 1
