096c037ec141d4c48efc368a6397655567e92f88
Eike <eike2d85@gmail.com>
2022-09-20 16:56:44 -0300

    coisa boa

--- main_compcalc.py
+++ main_compcalc.py
@@ -1,1 +1,19 @@
 import numpy as np
+from regmist_input import regmist_input
+
+#---------------DADOS DE ENTRADA---------------
+# Opções de entrada: 0-> Propriedades do material; 1->Regra das Misturas
+ent_opt = 0
+
+if ent_opt == 0:
+    E1=1
+    E2=1
+
+    inputs = np.array([E1, E2])
+elif ent_opt == 1:
+    Ef = 1
+    Em = 1
+    Vf = 0.5
+    inputs = regmist_input(Ef,Em, Vf)
+else:
+    print('Opção Inválida de entrada')
---
+++ regmist_input.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def regmist_input(Ef, Em, Vf):
+    E1 = 1
+    E2 = 2
+    inputs = np.array([E1, E2])
+    return inputs
