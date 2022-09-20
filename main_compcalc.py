aa60ee3d31b5956b6e48b2dea0f8c21fc480cc1d
Eike <eike2d85@gmail.com>
2022-09-20 17:23:20 -0300

    vai

--- main_compcalc.py
+++ main_compcalc.py
@@ -6,14 +6,16 @@ from regmist_input import regmist_input
 ent_opt = 0
 
 if ent_opt == 0:
-    E1=1
-    E2=1
-
-    inputs = np.array([E1, E2])
+    E1 = 1
+    E2 = 1
+    v12 = 1
+    G12 = 1
+    inputs = np.array([E1, E2, v12, G12])
 elif ent_opt == 1:
     Ef = 1
     Em = 1
     Vf = 0.5
-    inputs = regmist_input(Ef,Em, Vf)
+    inputs = regmist_input(Ef, Em, Vf)
 else:
     print('Opção Inválida de entrada')
+
--- regmist_input.py
+++ regmist_input.py
@@ -1,7 +1,11 @@
 import numpy as np
 
-def regmist_input(Ef, Em, Vf):
-    E1 = 1
-    E2 = 2
-    inputs = np.array([E1, E2])
+def regmist_input(Ef, Em, Vf, vf, vm, Gf, Gm): # (Mod. elasticidade; Mod Elasticidade, Fração volumética de fibra; Poisson fibra; Poisson Matriz)
+    Vm = 1 - Vf
+    E1 = Ef*Vf + Em*Vm
+    E2 = 1/((Vf/Ef)+(Vm/Em))
+    v12 = vf*Vf + vm*Vm
+    v21 = v12*E2/E1
+    G12 = 1/((Vf/Gf)+(Vm/Gm))
+    inputs = np.array([E1, E2, v12, G12])
     return inputs
